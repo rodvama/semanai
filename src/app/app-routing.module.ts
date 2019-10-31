333d9f49705541c73b9e0c44345b695492cd92b7
rodvama <rodvama@gmail.com>
2019-10-31 11:50:18 -0600

    carousel-almost-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { CarouselComponent } from './components/heimich/carousel/carousel.component';
 
 const routes: Routes = [
   {
     path:'',
     component:LandingPageComponent
+  },
+  {
+    path:'page1',
+    component: CarouselComponent
   }
 ];
 
